065eac886fe174536f6fff838e10195814198b67
admin <admin@DESKTOP-FA5KDQG>
2024-09-30 15:32:50 +0300

    Add project files.

---
+++ AdditionalTask1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_HW_28_09
+{
+    internal class AdditionalTask1
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = GetConnectionString();
+
+            int auctionID = 1;
+            int bidderID = 3;
+            string bidderName = "Jeorge";
+            decimal newBidAmount = 100.00m;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    string getCurrentBidQuery = "SELECT CurrentBid FROM Auctions WHERE AuctionID = @AuctionID";
+                    using (SqlCommand getCurrentBidCommand = new SqlCommand(getCurrentBidQuery, connection, transaction))
+                    {
+                        getCurrentBidCommand.Parameters.AddWithValue("@AuctionID", auctionID);
+                        decimal currentBid = (decimal)getCurrentBidCommand.ExecuteScalar();
+
+                        if (newBidAmount <= currentBid)
+                        {
+                            throw new Exception("Новая ставка должна быть выше текущей.");
+                        }
+                    }
+
+                    string checkBidderQuery = "SELECT COUNT(1) FROM Bidders WHERE BidderID = @BidderID";
+                    using (SqlCommand checkBidderCommand = new SqlCommand(checkBidderQuery, connection, transaction))
+                    {
+                        checkBidderCommand.Parameters.AddWithValue("@BidderID", bidderID);
+                        int bidderExists = (int)checkBidderCommand.ExecuteScalar();
+
+                        if (bidderExists == 0)
+                        {
+                            string insertBidderQuery = "INSERT INTO Bidders (BidderName) VALUES (@BidderName); SELECT SCOPE_IDENTITY();";
+                            using (SqlCommand insertBidderCommand = new SqlCommand(insertBidderQuery, connection, transaction))
+                            {
+                                insertBidderCommand.Parameters.AddWithValue("@BidderName", bidderName);
+                                bidderID = Convert.ToInt32(insertBidderCommand.ExecuteScalar());
+                            }
+                        }
+                    }
+
+                    string updateAuctionQuery = "UPDATE Auctions SET CurrentBid = @NewBid WHERE AuctionID = @AuctionID";
+                    using (SqlCommand updateAuctionCommand = new SqlCommand(updateAuctionQuery, connection, transaction))
+                    {
+                        updateAuctionCommand.Parameters.AddWithValue("@NewBid", newBidAmount);
+                        updateAuctionCommand.Parameters.AddWithValue("@AuctionID", auctionID);
+                        updateAuctionCommand.ExecuteNonQuery();
+                    }
+
+                    string insertBidQuery = @"
+                    INSERT INTO Bids (AuctionID, BidderID, BidAmount, BidTime)
+                    VALUES (@AuctionID, @BidderID, @BidAmount, @BidTime)";
+                    using (SqlCommand insertBidCommand = new SqlCommand(insertBidQuery, connection, transaction))
+                    {
+                        insertBidCommand.Parameters.AddWithValue("@AuctionID", auctionID);
+                        insertBidCommand.Parameters.AddWithValue("@BidderID", bidderID);
+                        insertBidCommand.Parameters.AddWithValue("@BidAmount", newBidAmount);
+                        insertBidCommand.Parameters.AddWithValue("@BidTime", DateTime.Now);
+                        insertBidCommand.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                    Console.WriteLine("Ставка успешно сделана.");
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine("Ошибка при совершении ставки: " + ex.Message);
+                }
+            }
+
+        }
+        private static string GetConnectionString()
+        {
+            var configurationBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+
+            return configuration.GetConnectionString("AuctionConnection");
+        }
+    }
+}
---
+++ accountTransaction.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = GetConnectionString();
+        string srcAccountNumber = "3";
+        string destAccountNumber = "2";
+        decimal transferAmount = 100.00m;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            SqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                string checkBalanceQuery = "SELECT Balance FROM Accounts WHERE AccountNumber = @AccountNumber";
+                using (SqlCommand command = new SqlCommand(checkBalanceQuery, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@AccountNumber", srcAccountNumber);
+                    decimal currentBalance = (decimal)command.ExecuteScalar();
+
+                    if (currentBalance < transferAmount)
+                    {
+                        throw new Exception("Недостаточно средств.");
+                    }
+                }
+                string withdrawQuery = "UPDATE Accounts SET Balance = Balance - @Amount WHERE AccountNumber = @AccountNumber";
+                using (SqlCommand command = new SqlCommand(withdrawQuery, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@Amount", transferAmount);
+                    command.Parameters.AddWithValue("@AccountNumber", srcAccountNumber);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new Exception("Во время операция списания средств произошла ошибка.");
+                    }
+                }
+
+                string depositQuery = "UPDATE Accounts SET Balance = Balance + @Amount WHERE AccountNumber = @AccountNumber";
+                using (SqlCommand command = new SqlCommand(depositQuery, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@Amount", transferAmount);
+                    command.Parameters.AddWithValue("@AccountNumber", destAccountNumber);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new Exception("Целевой счет не найден.");
+                    }
+                }
+
+                transaction.Commit();
+                Console.WriteLine("Перевод успешно выполнен.");
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine("Ошибка при выполнении транзакции: " + ex.Message);
+            }
+        }
+    }
+
+    private static string GetConnectionString()
+    {
+        var configurationBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+        var configuration = configurationBuilder.Build();
+
+        return configuration.GetConnectionString("DefaultConnection");
+    }
+}
